ec86239466293fc80e87a254ae7b075e9a8fe9f6
Seth Strouf <seth.strouf@doane.edu>
2022-06-05 14:10:15 -0500

    Finish working and refactoring palindrome method and tests

--- lib/strouf_palindrome.rb
+++ lib/strouf_palindrome.rb
@@ -1,6 +1,15 @@
 require "strouf_palindrome/version"
 
-module StroufPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
